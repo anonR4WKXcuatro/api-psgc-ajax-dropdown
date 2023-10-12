d0a369bce2006dc645f4ea34e081e166e7cb8750
Dexter <officialrawxkterr@yahoo.com>
2023-10-13 01:39:16 +0800

    Fix spellings

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
                 });
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                console.log('Errors:', errorThrown);
+                console.log('Error:', errorThrown);
             }
         });
     });
