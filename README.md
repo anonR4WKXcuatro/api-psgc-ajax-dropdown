ce645c18cd9a4e38ab8b480411b47f2445af6c5a
Dexter <officialrawxkterr@yahoo.com>
2023-10-13 01:37:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# api-psgc-ajax-dropdown
---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    // Populate the provinces dropdown on page load
+    $.ajax({
+        url: "https://psgc.gitlab.io/api/island-groups/luzon/provinces/",
+        type: "GET",
+        success: function (data) {
+            var options = JSON.parse(data);
+            var dropdown = $("#provinces_drop_down");
+
+            $.each(options, function (index, value) {
+                dropdown.append("<option value='" + value.code + "'>" + value.name + "</option>");
+            });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('Error:', errorThrown);
+        }
+    });
+
+    // Detect when the provinces dropdown changes and populate the cities dropdown
+    $("#provinces_drop_down").change(function () {
+        var provinceCode = $(this).val();
+
+        var citiesDropdown = $("#cities_municipalities_drop_down");
+
+        $.ajax({
+            url: "https://psgc.gitlab.io/api/provinces/" + provinceCode + "/cities-municipalities/",
+            type: "GET",
+            success: function (data) {
+                var options = JSON.parse(data);
+                citiesDropdown.empty(); // clear the cities dropdown
+
+                $.each(options, function (index, value) {
+                    citiesDropdown.append("<option value='" + value.code + "'>" + value.name + "</option>");
+                });
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('Errors:', errorThrown);
+            }
+        });
+    });
+});
